1c439f1353f5c13f59a27220d50e62de4446393e
morgane <mcabrol21@gmail.com>
2015-12-29 15:16:47 +0100

    form

--- src/jeuMenhir/InterfaceGraphique/PanelFormulaire.java
+++ src/jeuMenhir/InterfaceGraphique/PanelFormulaire.java
@@ -1,15 +1,11 @@
 package jeuMenhir.InterfaceGraphique;
 
 import javax.imageio.ImageIO;
-import javax.imageio.plugins.jpeg.JPEGHuffmanTable;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import  javax.swing.*;
-import  java.awt.*;
-import  java.awt.event.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -19,23 +15,51 @@ import java.io.IOException;
  */
 public class PanelFormulaire extends JPanel {
     private FenetreFormulaire fenetreForm;
-    private JButton jouerPartiRap;
-    private JButton jouerPartiAvance;
+    private JComboBox comboNbrJTot;
+    private JLabel labelNbrJTot;
+
+    private JComboBox comboNbrJOrdi;
+    private JLabel labelNbrJOrdi;
 
 
     public PanelFormulaire(FenetreFormulaire f){
         this.fenetreForm=  f;
         this.repaint();
-        jouerPartiRap = new JButton("Commencer une partie rapide ");
-        jouerPartiAvance = new JButton("Commencer une partie avancée");
-        jouerPartiRap.addActionListener(this.fenetreForm);
 
-        this.add(jouerPartiRap);
+        labelNbrJTot = new JLabel("Selectionner le nombre de joueurs au total ");
+        comboNbrJTot = new JComboBox();
+        comboNbrJTot.setPreferredSize(new Dimension(100, 20));
+        String[] tab = {"1", "2", "3", "4","5","6","7","8"};
 
-        jouerPartiAvance.addActionListener(this.fenetreForm);
+        comboNbrJTot = new JComboBox(tab);
+        this.add(labelNbrJTot);
+        this.add(comboNbrJTot);
+        comboNbrJTot.addItemListener(new ItemState());
 
-        this.add(jouerPartiAvance);
     }
 
+    public void paintComponent(Graphics g){
+        BufferedImage menu = null;
 
+        try {
+            menu = ImageIO.read(new File("Images/imageDebut.jpg"));
+
+        } catch (IOException ex) {
+            System.out.print("non chargé");
+        }
+        g.drawImage(menu,0,0,this.getWidth(),this.getHeight(),null);
+
+
+
+
+    }
+
+   class ItemState implements ItemListener {
+
+       public void itemStateChanged(ItemEvent e) {
+
+           System.out.println("événement déclenché sur : " + e.getItem());
+       }
+
+   }
 }
